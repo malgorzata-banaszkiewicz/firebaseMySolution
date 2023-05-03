af404819c40ba4db75e9fbd0a5c3be4bc4d72215
Małgorzata Banaszkiewicz <malnutelka@gmail.com>
2023-05-03 11:37:40 +0200

    changed list into nice table

--- my-international-school/src/components/Students/Students.jsx
+++ my-international-school/src/components/Students/Students.jsx
@@ -1,3 +1,4 @@
+
 import { getStudents } from '../../firebase/getStudents';
 import db from '../../firebase/firebase.config'
 import { onSnapshot, collection, addDoc, deleteDoc, doc, updateDoc} from 'firebase/firestore';
@@ -56,87 +57,128 @@ const handleChange = async (e, id) => {
       }, [])
     return(
       
-      <>
-      <Outlet></Outlet>
-      <form onSubmit={handleSubmit} className={styles.breathe}>
-        <h2>Add a new student</h2>
-        <div className={styles.breathe}>
-          <label htmlFor="studentID">Student's ID: </label>
+      <div className={styles.all}>
+        <Outlet></Outlet>
+        <form onSubmit={handleSubmit} className={styles.breathe}>
+          <h2>Add a new student</h2>
+          <div className={styles.breathe}>
+            <label htmlFor="studentID">Student's ID: </label>
             <input type="text" name="studentID" id="studentID"></input>
-        </div>
-        <div className={styles.breathe}>
-          <label htmlFor="firstName">Student's first name: </label>
-          <input type="text" name="firstName" id="firstName"></input>
-        </div>
-        <div className={styles.breathe}>
-          <label htmlFor="lastName">Student's last name: </label>
-          <input type="text" name="lastName" id="lastName"></input>
-        </div>
-        <div className={styles.breathe}>
-          <label htmlFor="grade">Student's grade: </label>
-          <input type="text" name="grade" id="grade"></input>
-        </div>
-        <div className={styles.breathe}>
-          <label htmlFor="placeOfResidence">Student's Place of residence: </label>
-          <input type="text" name="placeOfResidence" id="placeOfResidence"></input>
-        </div>
-        <div className={styles.breathe}>
-          <label htmlFor="yearOfBirth">Student's Year of birth: </label>
-          <input type="text" name="yearOfBirth" id="yearOfBirth"></input>
-        </div>
-        <button className={styles.borderPink}>Add new student</button>
-      </form>
-      <h2 className={styles.important}>List of current students</h2>
-        <ul>
-        {students.map(({ id, studentID, firstName, lastName, grade, placeOfResidence, yearOfBirth}) => 
-        (
-          <li data-id={id} key={id}> 
-          {temporaryID != `edit ${id}` ? (
-            <>
-          <p className={styles.important}>Student ID: {studentID}</p>
-          <p>First Name: {firstName}</p>
-          <p>Last name: {lastName}</p>
-          <p>Grade: {grade}</p>
-          <p>Place of residence: {placeOfResidence}</p>
-          <p>Year of birth: {yearOfBirth}</p>
-          <button onClick={() => handleDelete(id)} className={`${styles.delete}  ${styles.breathe}`}>Delete student</button>
-          <button onClick={() => setTemporaryID(`edit ${id}`)} className={`${styles.borderGrey} ${styles.breathe}`}>Edit student</button>
-          <button className={`${styles.borderOrange} ${styles.breathe}`}>View student's page</button>
-          </>
-          ) : (
+          </div>
+          <div className={styles.breathe}>
+            <label htmlFor="firstName">Student's first name: </label>
+            <input type="text" name="firstName" id="firstName"></input>
+          </div>
+          <div className={styles.breathe}>
+            <label htmlFor="lastName">Student's last name: </label>
+            <input type="text" name="lastName" id="lastName"></input>
+          </div>
+          <div className={styles.breathe}>
+            <label htmlFor="grade">Student's grade: </label>
+            <input type="text" name="grade" id="grade"></input>
+          </div>
+          <div className={styles.breathe}>
+            <label htmlFor="placeOfResidence">Student's Place of residence: </label>
+            <input type="text" name="placeOfResidence" id="placeOfResidence"></input>
+          </div>
+          <div className={styles.breathe}>
+            <label htmlFor="yearOfBirth">Student's Year of birth: </label>
+            <input type="text" name="yearOfBirth" id="yearOfBirth"></input>
+          </div>
+          <button className={styles.borderPink}>Add new student</button>
+        </form>
+
+        <h2 className={styles.important}>
+            List of current students
+        </h2>
+        <div className={styles.students}>
+          {students.map(({ id, studentID, firstName, lastName, grade, placeOfResidence, yearOfBirth}) => 
+          (
+            
+          <div data-id={id} key={id}> 
+            {temporaryID != `edit ${id}` ? (
+            <table className={styles.table}>
+              <thead className={styles.thead}> 
+                <tr>
+                  <th scope="column" colSpan={3}>STUDENT</th>
+                </tr>
+              </thead>
+              <tbody className={styles.tbody}>
+                <tr>
+                  <td>Student ID:</td>
+                  <td colSpan={2}>{studentID}</td>
+                </tr>
+                <tr>
+                  <td>First Name:</td>
+                  <td colSpan={2}>{firstName}</td>
+                </tr>
+                <tr>
+                  <td>Last name:</td>
+                  <td colSpan={2}>{lastName}</td>
+                </tr>
+                <tr>
+                  <td>Grade:</td>
+                  <td colSpan={2}>{grade}</td>
+                </tr>
+                <tr>
+                  <td>Place of residence:</td>
+                  <td colSpan={2}>{placeOfResidence}</td>
+                </tr>
+                <tr>
+                  <td>Year of birth:</td>
+                  <td colSpan={2}>{yearOfBirth}</td>
+                </tr>
+              </tbody>
+              <tfoot >
+                <tr>
+                  <td className={styles.tfoot}>
+                    <button onClick={() => handleDelete(id)} className={`${styles.delete}  ${styles.breathe}`}>Delete student</button>
+                  </td>
+                  <td className={styles.tfoot}>
+                    <button onClick={() => setTemporaryID(`edit ${id}`)} className={`${styles.borderGrey} ${styles.breathe}`}>Edit student</button>
+                  </td>
+                  <td className={styles.tfoot}> 
+                    <button className={`${styles.borderOrange} ${styles.breathe}`}>Details</button>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+            ) : (
             <form onSubmit={e => handleChange(e, id)} >
-             <div >
-          <label htmlFor="studentID">Student's ID:</label>
-            <input type="text" name="studentID" id="studentID" defaultValue={studentID}></input>
-        </div>
-        <div>
-          <label htmlFor="firstName">Student's first name:</label>
-          <input type="text" name="firstName" id="firstName" defaultValue={firstName}></input>
-        </div>
-        <div>
-          <label htmlFor="lastName">Student's last name:</label>
-          <input type="text" name="lastName" id="lastName" defaultValue={lastName}></input>
-        </div>
-        <div>
-          <label htmlFor="grade">Student's grade:</label>
-          <input type="text" name="grade" id="grade" defaultValue={grade}></input>
-        </div>
-        <div>
-          <label htmlFor="placeOfResidence">Student's Place of residence:</label>
-          <input type="text" name="placeOfResidence" id="placeOfResidence" defaultValue={placeOfResidence}></input>
-        </div>
-        <div>
-          <label htmlFor="yearOfBirth">Student's Year of birth:</label>
-          <input type="text" name="yearOfBirth" id="yearOfBirth" defaultValue={yearOfBirth}></input>
-        </div>
-        <button type="submit">Confirm</button>
-        <button type="button" onClick={() => setTemporaryID(null)}>Cancel</button>
+              <div>
+                <label htmlFor="studentID">Student's ID:</label>
+                <input type="text" name="studentID" id="studentID" defaultValue={studentID}></input>
+              </div>
+              <div>
+                <label htmlFor="firstName">Student's first name:</label>
+                <input type="text" name="firstName" id="firstName" defaultValue={firstName}></input>
+              </div>
+              <div>
+                <label htmlFor="lastName">Student's last name:</label>
+                <input type="text" name="lastName" id="lastName" defaultValue={lastName}></input>
+              </div>
+              <div>
+                <label htmlFor="grade">Student's grade:</label>
+                <input type="text" name="grade" id="grade" defaultValue={grade}></input>
+              </div>
+              <div>
+                <label htmlFor="placeOfResidence">Student's Place of residence:</label>
+                <input type="text" name="placeOfResidence" id="placeOfResidence" defaultValue={placeOfResidence}></input>
+              </div>
+              <div>
+                <label htmlFor="yearOfBirth">Student's Year of birth:</label>
+                <input type="text" name="yearOfBirth" id="yearOfBirth" defaultValue={yearOfBirth}></input>
+              </div>
+              <button type="submit">Confirm</button>
+              <button type="button" onClick={() => setTemporaryID(null)}>Cancel</button>
             </form>
+            )}
+          </div>
+          )
           )}
-          </li>
-        )
-        )}
-      </ul>
-      </>
+        </div>
+      </div>
+      
     )
 }
+
